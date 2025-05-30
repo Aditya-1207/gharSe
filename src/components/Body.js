1d067f2d1f77935d9119b2f5fc145e5334f70ad4
Aditya-1207 <108715826+Aditya-1207@users.noreply.github.com>
2025-05-30 18:30:48 +0530

    Added filter button for React useState hooks assignment

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,39 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../../common/mockData";
+import { useState } from "react";
+
 
 //Body component to have restaurant cards
 
 const Body = () => {
+
+     const [listOfRestaurants, setlistOfRestaurants] = useState(restaurantList);
+
     return (
         <div className="body">
             <h3>This is the body</h3>
             <div className="search-container">
                 <input className="search-bar" placeholder="Search for restaurants, cuisines..." />
             </div>
+            {/* Added a top rated filter button using React Hooks: useState function */}
+            <div className="topRatedFilter">
+                <button className="topRatedButton" onClick={ () => {
+                    // Filter the list and set it to useState function
+                    const filteredList = listOfRestaurants.filter(
+                        (restaurant) => {
+                            return restaurant.info.avgRating>4.0
+                        }
+                    );
+                    setlistOfRestaurants(filteredList);
+                }
+                }>Top Rated Restaurants</button>
+            </div>
             <div className="restaurant-container">                
                 {/* Map to iterate on the restaurant list.
-                Key is important from React 19 so thatReact can track which items changed between re-renders in order to maintain 
+                Key is mandatory from React 19 so that React can track which items changed between re-renders in order to maintain 
                 the virtual DOM diffing. */}
-                {restaurantList.map((restaurant) => (
+
+                {listOfRestaurants.map((restaurant) => (
                     <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                 )
                 )};
